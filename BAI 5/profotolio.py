92ca891146f49adc0e2f08564f7e06a5248ba592
MinhThuyet <mthuyet123@gmail.com>
2025-11-22 19:54:19 +0700

    Updated

---
+++ BAI 5/profotolio.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Giới thiệu bản thân")
+
+st.write("🧑 Họ và tên: Vũ Trần Minh Thuyết")
+st.write("🎂 Ngày tháng năm sinh: 25/09/2010")
+st.write("📘 Môn học yêu thích: Toán")
+st.write("🏸 Sở thích: chơi cầu lông và ♟️ cờ vua")
