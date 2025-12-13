153c79a234653c52b03d130f9d3e6e81e65e05ed
MinhThuyet <mthuyet123@gmail.com>
2025-12-13 20:25:05 +0700

    Updated

--- BAI 8/fav_singer.py
+++ BAI 8/fav_singer.py
@@ -12,7 +12,7 @@ with st.sidebar:
 
 st.title('Bài hát yêu thích')
 st.write('As It Was')
-audio_file = open('source/asitwas.mp3', 'rb')
+audio_file = open('BAI 8/source/asitwas.mp3', 'rb')
 st.audio(audio_file, format='audio/mp3')
 
 st.title('MV yêu thích')
