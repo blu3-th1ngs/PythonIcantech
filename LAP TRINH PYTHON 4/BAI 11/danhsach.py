fa452b50afe7a12d765ad9b595fef654bb84b79f
MinhThuyet <mthuyet123@gmail.com>
2026-01-10 20:17:23 +0700

    Updated

---
+++ LAP TRINH PYTHON 4/BAI 11/danhsach.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+with st.form('Order đồ uống'):
+    drinks = ('Trà sữa truyền thống', 'Trà sữa matcha', 'Trà sữa trái cây', 'Trà sữa socola', 'Trà sữa khoai môn', 'Trà sữa oolong')
+    option_drink = st.selectbox('Bạn muốn loại đồ uống gì?', drinks)
+    
+    sugars = ('Đường trắng', 'Đường nâu', 'Ít đường', 'Không đường')
+    option_sugar = st.selectbox('Bạn thích thêm loại đường nào?', sugars)
+    
+    jellys = ('Thạch rau câu', 'Thạch nha đam', 'Trân châu trắng', 'Trân châu đen', 'Thạch café', 'Không thêm thạch')
+    option_jelly = st.selectbox('Bạn thích thêm loại thạch nào?', jellys)
+    
+    sizes = ('Nhỏ', 'Vừa', 'Lớn', 'Đại')
+    option_size = st.selectbox('Bạn muốn size nào?', sizes)
+    
+    toppings = ('Không thêm topping', 'Kem cheese', 'Kem trứng', 'Đậu đỏ', 'Thạch phô mai')
+    option_topping = st.selectbox('Bạn muốn thêm topping gì?', toppings)
+    
+    ice_options = ('Nhiều đá', 'Ít đá', 'Không đá', 'Đá riêng')
+    option_ice = st.selectbox('Bạn muốn đá như thế nào?', ice_options)
+    
+    nums = st.slider('Số lượng bạn muốn đặt:', 0, 10, 1)
+    
+    bill = {
+        'Loại đồ uống:': option_drink,
+        'Loại đường:': option_sugar,
+        'Loại thạch:': option_jelly,
+        'Size:': option_size,
+        'Topping:': option_topping,
+        'Đá:': option_ice,
+        'Số lượng:': nums
+    }
+    
+    submitted = st.form_submit_button("Xác nhận")
+    
+    if submitted:
+        st.write('Bạn đã chọn:')
+        for x, y in bill.items():
+            st.write(x, y)
+    
+    print_bill = st.checkbox('In hoá đơn')
+    
+    if print_bill:
+        ans = ''
+        for x in bill:
+            ans += str(x) + ' ' + str(bill[x]) + '\n'
+        st.download_button('Tải hoá đơn', ans, file_name='hoa_don.txt')
